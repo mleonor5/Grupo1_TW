8410df9ac9a9cd7f6623d383c18c06ef12620fec
fabiofernandes2002 <varelafernandesfabio12@gmail.com>
2021-12-29 01:31:43 +0000

    login e registo.js que acrescentei

---
+++ js/login.js
@@ -0,0 +1,21 @@
+let utilizadores = [
+    {"login": "", "marialeonor@gamil.com": "leonormaria"},
+    {"login": "fabiofernandes@gmail.com", "senha": "fernandes"},
+    {"login": "joaorocha@gmail.com", "senha": "rochajoao"},
+];
+
+function login() {
+    let email = document.getElementsById('txtEmail')[0].value.toLowerCase();
+    let password = document.getElementsById('txtPassword')[0].value;
+
+    for (let u in utilizadores) {
+        let us = utilizadores[u];
+        if (us.login === email && us.password === password) {
+            alert('Início de sessão efetuado com sucesso!')
+            window.location = "index.html";
+            return true;        
+        }
+    }
+    alert("Dados incorretos, tente novamente.");
+    return false;
+}
---
+++ js/registo.js
@@ -0,0 +1,3 @@
+function registo() {
+    alert("Registo com sucesso!")
+}

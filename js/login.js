b47d0851e3216d0838faebff0f6e6b615f7469a3
Maria Rodrigues <mleonorspr@gmail.com>
2022-01-20 21:00:56 +0000

    gestão de página de utilizador

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 let utilizadores = [
-    {login: "marialeonor@gmail.com", senha: "leonormaria"},
+    {nome:"Leonor", login: "marialeonor@gmail.com", genero:"Feminino", morada:"Vila do Conde", telemovel:"123456789", senha: "leonormaria"},
     {login: "fabiofernandes@gmail.com", senha: "fernandes"},
     {login: "joaorocha@gmail.com", senha: "rochajoao"},
 ];
@@ -11,9 +11,22 @@ form.addEventListener('submit', function(event){
     let email = document.querySelector('#txtEmail').value
     let pass = document.querySelector('#txtPassword').value
 
+    let editName = document.querySelector('#editName')
+    let editEmail = document.querySelector('#editEmail')
+    let editGenero = document.querySelector('#editGender')
+    let editMorada = document.querySelector('#editMorada')
+    let editTelemovel = document.querySelector('#editNumber')
+    let editPass = document.querySelector('#editPassword')
+
     for (let user in utilizadores){
         if(utilizadores[user].login == email && utilizadores[user].senha == pass){
-            alert(`Bem-vindo ${email}`)
+            alert(`Bem-vindo ${utilizadores[user].nome}`)
+            editName.value = utilizadores[user].nome
+            editEmail.value = utilizadores[user].login
+            editGenero.value = utilizadores[user].genero
+            editMorada.value = utilizadores[user].morada
+            editTelemovel.value = utilizadores[user].telemovel
+            editPass.value = utilizadores[user].senha
             window.location = '../index.html'
             break
         }else{

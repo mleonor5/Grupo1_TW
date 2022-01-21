2910f224640447385e915b57a1f44b24cb73cd59
Maria Rodrigues <mleonorspr@gmail.com>
2022-01-21 21:12:08 +0000

    login

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 let utilizadores = [
     {nome:"Leonor", login: "marialeonor@gmail.com", genero:"Feminino", morada:"Vila do Conde", telemovel:"123456789", senha: "leonormaria"},
     {nome:"Fabio", login: "fabiofernandes@gmail.com", genero:"Masculino", morada:"Vila de Conde", telemovel:"234567890", senha: "fernandes"},
-    {nome: "João", login: "joaorocha@gmail.com", genero:"Masculino", morada:"Vila de Conde", senha: "rochajoao"},
+    {nome: "João", login: "joaorocha@gmail.com", genero:"Masculino", morada:"Vila de Conde", telemovel:"345678901", senha: "rochajoao"},
 ];
 
 let form = document.querySelector('.form')
@@ -10,14 +10,18 @@ form.addEventListener('submit', function(event){
 
     let email = document.querySelector('#txtEmail').value
     let pass = document.querySelector('#txtPassword').value
+    let helloUser = document.querySelector('#helloUser')
+    let loginText = document.querySelector('#loginText')
 
     for (let user in utilizadores){
         if(utilizadores[user].login == email && utilizadores[user].senha == pass){
-            alert(`Bem-vindo ${utilizadores[user].nome}`)
-            window.location = '../index.html'
+            helloUser.innerHTML = `Bem-vindo ${utilizadores[user].nome}`
+            window.setTimeout(function(){
+                window.location = '../index.html'
+            }, 5000);
             break
         }else{
-            alert(`Dados incorretos, tente novamente`)
+            loginText.innerHTML = 'Palavra-passe ou Email incorretos!'
         }
     }
 
